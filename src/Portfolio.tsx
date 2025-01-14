098b1aadcae18c9e9d1533ad7316c7010afee969
Dagmawi Nebiat <dagmnebiat@gmail.com>
2025-01-14 18:50:42 +0300

    First Commit

---
+++ src/Portfolio.tsx
@@ -0,0 +1,11 @@
+const Portfolio = () => {
+  return (
+    <main className="h-screen grid justify-center items-center">
+      <div className="border-2 border-black h-64 w-64 rounded-full grid items-center justify-center hover:scale-125 duration-[1000ms] ease-in-out">
+        <p className="text-xl text-center w-24">Main Avatar Placeholder</p>
+      </div>
+    </main>
+  );
+};
+
+export default Portfolio;
