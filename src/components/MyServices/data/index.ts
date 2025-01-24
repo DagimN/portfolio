d5879128eac5542661d4e9493ca8e8bd6c12f92f
Dagmawi Nebiat <dagmnebiat@gmail.com>
2025-01-24 15:15:11 +0300

    service card hover animation partially implemented

--- src/components/MyServices/data/index.ts
+++ src/components/MyServices/data/index.ts
@@ -9,30 +9,30 @@ export default [
     title: "Backend Servers",
     description: "",
     image:
-      "https://media.licdn.com/dms/image/D5612AQFwV7zzF8PreQ/article-cover_image-shrink_720_1280/0/1691465286767?e=2147483647&v=beta&t=glX0RA_O8iJ1zU1rwTikBf_oGzV9csXunhc-PwZbPnU",
+      "https://www.inovex.de/wp-content/uploads/Blogheader-Bring-Your-Own-Backend-720x520.png",
   },
   {
     title: "Full Stack",
     description: "",
     image:
-      "https://media.licdn.com/dms/image/D5612AQFwV7zzF8PreQ/article-cover_image-shrink_720_1280/0/1691465286767?e=2147483647&v=beta&t=glX0RA_O8iJ1zU1rwTikBf_oGzV9csXunhc-PwZbPnU",
+      "https://cnelindia.com/blog/wp-content/uploads/2024/10/Designer-12.jpeg",
   },
   {
     title: "Mobile Development",
     description: "",
     image:
-      "https://media.licdn.com/dms/image/D5612AQFwV7zzF8PreQ/article-cover_image-shrink_720_1280/0/1691465286767?e=2147483647&v=beta&t=glX0RA_O8iJ1zU1rwTikBf_oGzV9csXunhc-PwZbPnU",
+      "https://www.smartdigitalwings.com/assets/img/mobile-app-development-in-meerut.jpg",
   },
   {
     title: "Desktop Development",
     description: "",
     image:
-      "https://media.licdn.com/dms/image/D5612AQFwV7zzF8PreQ/article-cover_image-shrink_720_1280/0/1691465286767?e=2147483647&v=beta&t=glX0RA_O8iJ1zU1rwTikBf_oGzV9csXunhc-PwZbPnU",
+      "https://digitalmedia16.com/wp-content/uploads/2024/06/digitalmedia16-monthly-maintenance.jpg",
   },
   {
     title: "Bots i.e. Telegram/Discord",
     description: "",
     image:
-      "https://media.licdn.com/dms/image/D5612AQFwV7zzF8PreQ/article-cover_image-shrink_720_1280/0/1691465286767?e=2147483647&v=beta&t=glX0RA_O8iJ1zU1rwTikBf_oGzV9csXunhc-PwZbPnU",
+      "https://gpttutorpro.com/wp-content/uploads/2024/03/a-robot-is-developing-a-tutorial-with-the-title-building-chatbots-with-dialogflow-analyzing-and-improving-your-chatbot-performance.jpeg",
   },
 ];
--- src/components/MyServices/index.tsx
+++ src/components/MyServices/index.tsx
@@ -1,9 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ServiceCard from "./components/ServiceCard";
 import data from "./data";
 
 const Services = () => {
-  const [serviceIndex, _setServiceIndex] = useState(0);
+  const [serviceIndex, setServiceIndex] = useState(0);
+  const [hoverIndex, setHoverIndex] = useState<number>();
+  const timers: number[] = [];
+
+  useEffect(() => {
+    if (hoverIndex) return;
+
+    const timer = setTimeout(() => {
+      //TODO: Clear timers when back to first
+      setServiceIndex((serviceIndex + 1) % data.length);
+    }, 5000);
+
+    timers.push(timer);
+  }, [serviceIndex]);
 
   return (
     <section className="relative h-[820px] w-full bg-black">
@@ -23,12 +36,33 @@ const Services = () => {
         <h1 className="text-5xl text-saturated-green font-bold my-5">
           Services
         </h1>
-        <div className="bg-gray-400 w-full h-[75%] bg-opacity-30 backdrop-blur-sm  rounded-[50px] border-primary border-opacity-10 border-[1px]">
+        <div className="bg-gray-400 w-full h-[75%] bg-opacity-30 backdrop-blur-sm  rounded-[50px] border-primary border-opacity-10 border-[1px] relative">
           <ServiceCard
-            title={data[serviceIndex].title}
-            description={data[serviceIndex].description}
-            image={data[serviceIndex].image}
+            title={data[hoverIndex ?? serviceIndex].title}
+            description={data[hoverIndex ?? serviceIndex].description}
+            image={data[hoverIndex ?? serviceIndex].image}
           />
+
+          <div className="absolute right-[17%] bottom-5 flex gap-5 z-10">
+            {data.map((_service, index) => (
+              <div
+                onMouseEnter={() => setHoverIndex(index)}
+                onMouseLeave={() => {
+                  setHoverIndex(undefined);
+                  var highestTimeoutId = setTimeout(";");
+                  for (var i = 0; i < highestTimeoutId; i++) {
+                    clearTimeout(i);
+                  }
+                  setServiceIndex(index);
+                }}
+                className={`h-3 w-3 ${
+                  index === (hoverIndex ?? serviceIndex)
+                    ? "bg-white"
+                    : "bg-white/50"
+                } rounded-full hover:scale-150 duration-300`}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </section>
